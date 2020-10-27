d94b982ec5a22ca9be9690944ea44e082b9452fd
Giosue1994 <giosuelentini1994@outlook.com>
2020-10-27 01:18:32 +0100

    gsap-smooth scroll

--- dist/app.js
+++ dist/app.js
@@ -99,55 +99,84 @@
 //   damping: 0.05,
 // }
 //
-// let wrapScrollbar = document.querySelector('#wrapper');
-//
 // Scrollbar.init(wrapScrollbar, options);
+gsap.registerPlugin(ScrollTrigger);
+var wrapScrollbar = document.querySelector('#wrapper');
 var sedia = $('#sedia');
 var poltrona = $('#poltrona');
 var treSedie = $('#tre-sedie');
 var spalliere = $('#spalliere');
+var locoScroll = new LocomotiveScroll({
+  el: wrapScrollbar,
+  smooth: true
+});
+locoScroll.on("scroll", ScrollTrigger.update);
+ScrollTrigger.scrollerProxy(wrapScrollbar, {
+  scrollTop: function scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect: function getBoundingClientRect() {
+    return {
+      top: 0,
+      left: 0,
+      width: window.innerWidth,
+      height: window.innerHeight
+    };
+  },
+  pinType: wrapScrollbar.style.transform ? "transform" : "fixed"
+});
 var tl = gsap.timeline();
 tl.to(spalliere, {
-  yPercent: -200
+  y: -820
 }).to(poltrona, {
-  yPercent: -200
+  y: -820
 }).to(treSedie, {
-  yPercent: -200
+  y: -820
 }).to(sedia, {
-  yPercent: -200
+  y: -820
+});
+tl.from(".title", {
+  scale: 1,
+  autoAlpha: 0
 });
 ScrollTrigger.create({
   animation: tl,
   trigger: '#img-wrapper',
   // markers: true,
-  start: '-200px top',
-  end: function end() {
-    return '+=' + document.querySelector('#wrapper').offsetHeight;
-  },
+  start: 'top top',
+  end: '+=100%',
+  // end: () => '+=' + wrapScrollbar.offsetHeight,
+  scroller: wrapScrollbar,
   scrub: true,
   pin: '#jumbo'
 });
+ScrollTrigger.addEventListener('refresh', function () {
+  return locoScroll.update();
+});
+ScrollTrigger.refresh();
 $(document).ready(function () {
-  $(window).scroll(function () {
-    // VARIABILI
-    var logo = $('#logo img');
-    var sedia = $('#sedia');
-    var poltrona = $('#poltrona');
-    var treSedie = $('#tre-sedie');
-    var spalliere = $('#spalliere'); // allo scroll della pagina,
-    // se i pixel sono minore di 200 nascondo il logo
-    // altrimenti torna come prima
-
-    if ($(this).scrollTop() > 900) {
-      logo.css({
-        opacity: '0'
-      });
-    } else {
-      logo.css({
-        opacity: '1'
-      });
-    }
-  });
+  $('#wrapper').css({
+    'overflow-y': 'hidden'
+  }); // $(window).scroll( function() {
+  //   // VARIABILI
+  //   let logo = $('#logo img');
+  //   let sedia = $('#sedia');
+  //   let poltrona = $('#poltrona');
+  //   let treSedie = $('#tre-sedie');
+  //   let spalliere = $('#spalliere');
+  // // allo scroll della pagina,
+  // // se i pixel sono minore di 200 nascondo il logo
+  // // altrimenti torna come prima
+  // if ($(this).scrollTop() > 900) {
+  //   logo.css({
+  //     opacity: '0',
+  //   });
+  // } else {
+  //   logo.css({
+  //     opacity: '1',
+  //   });
+  // }
+  // });
 });
 
 /***/ }),
--- src/app.js
+++ src/app.js
@@ -4,55 +4,80 @@
 //   damping: 0.05,
 // }
 //
-// let wrapScrollbar = document.querySelector('#wrapper');
-//
 // Scrollbar.init(wrapScrollbar, options);
+gsap.registerPlugin(ScrollTrigger);
 
-
+let wrapScrollbar = document.querySelector('#wrapper');
 let sedia = $('#sedia');
 let poltrona = $('#poltrona');
 let treSedie = $('#tre-sedie');
 let spalliere = $('#spalliere');
 
-const tl = gsap.timeline();
-tl.to(spalliere, {yPercent: -200})
-.to(poltrona, {yPercent: -200})
-.to(treSedie, {yPercent: -200})
-.to(sedia, {yPercent: -200});
+const locoScroll = new LocomotiveScroll({
+    el: wrapScrollbar,
+    smooth: true,
+});
+
+
+locoScroll.on("scroll", ScrollTrigger.update);
+
+ScrollTrigger.scrollerProxy(wrapScrollbar, {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  },
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  pinType: wrapScrollbar.style.transform ? "transform" : "fixed"
+});
+
+let tl = gsap.timeline();
+tl.to(spalliere, {y: -820})
+  .to(poltrona, {y: -820})
+  .to(treSedie, {y: -820})
+  .to(sedia, {y: -820});
+tl.from(".title", {scale: 1, autoAlpha: 0})
 
 ScrollTrigger.create({
   animation: tl,
   trigger: '#img-wrapper',
   // markers: true,
-  start: '-200px top',
-  end: () => '+=' + document.querySelector('#wrapper').offsetHeight,
+  start: 'top top',
+  end: '+=100%',
+  // end: () => '+=' + wrapScrollbar.offsetHeight,
+  scroller: wrapScrollbar,
   scrub: true,
   pin: '#jumbo',
 });
 
+ScrollTrigger.addEventListener('refresh', () => locoScroll.update());
+ScrollTrigger.refresh();
+
 $(document).ready(function() {
 
-  $(window).scroll( function() {
-    // VARIABILI
-    let logo = $('#logo img');
-    let sedia = $('#sedia');
-    let poltrona = $('#poltrona');
-    let treSedie = $('#tre-sedie');
-    let spalliere = $('#spalliere');
-
-    // allo scroll della pagina,
-    // se i pixel sono minore di 200 nascondo il logo
-    // altrimenti torna come prima
-    if ($(this).scrollTop() > 900) {
-      logo.css({
-        opacity: '0',
-      });
-    } else {
-      logo.css({
-        opacity: '1',
-      });
-    }
-
-
+  $('#wrapper').css({
+    'overflow-y': 'hidden'
   });
+
+  // $(window).scroll( function() {
+  //   // VARIABILI
+  //   let logo = $('#logo img');
+  //   let sedia = $('#sedia');
+  //   let poltrona = $('#poltrona');
+  //   let treSedie = $('#tre-sedie');
+  //   let spalliere = $('#spalliere');
+
+    // // allo scroll della pagina,
+    // // se i pixel sono minore di 200 nascondo il logo
+    // // altrimenti torna come prima
+    // if ($(this).scrollTop() > 900) {
+    //   logo.css({
+    //     opacity: '0',
+    //   });
+    // } else {
+    //   logo.css({
+    //     opacity: '1',
+    //   });
+    // }
+  // });
 });
